9ce994410505fc1e20a35e1517260d1660b6a809
Larissa Mussoi <larimu97@gmail.com>
2025-12-30 14:59:07 -0300

    Commit inicial da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <>
+      <aside>
+        <img src={logo} alt="Logo" />
+      </aside>
+    </>
+  );
+}
+
+export default App;
